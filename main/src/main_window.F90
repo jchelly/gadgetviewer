3467d1236903c4d633bcadc75a6b285fcea599bc
John Helly <j.c.helly@durham.ac.uk>
2025-04-10 15:50:26 +0100

    Don't create stereo separation slider until needed
    This avoids affecting the initial window size in gtk-3.

--- main/src/main_window.F90
+++ main/src/main_window.F90
@@ -173,8 +173,9 @@ module main_window
   ! Optional extra libraries
   integer, parameter :: nextra = 3
   character(len=100), dimension(nextra) :: extra
-  
+
   ! Stereo separation control
+  logical :: stereo_created = .false.
   type (gui_box)    :: stereo_box
   type (gui_label)  :: label
   type (gui_slider) :: stereo_slider
@@ -190,6 +191,27 @@ module main_window
   character(len=80) :: plot_lib
 
 contains
+
+  subroutine stereo_create()
+
+    implicit none
+    real :: sep
+
+    if(stereo_created)return
+
+    ! Add slider for stereo separation
+    call gui_create_box(stereo_box, toolbar, gui_horizontal)
+    call gui_create_label(label, stereo_box, "Stereo sep. ")
+    call gui_create_slider(stereo_slider, stereo_box, &
+         range=(/ stereo_min_sep, stereo_max_sep/), &
+         step=stereo_sep_step, orientation=gui_horizontal, min_size=100)
+    call gui_slider_set_value(stereo_slider,stereo_default_sep)
+    call gui_slider_get_value(stereo_slider,sep)
+    call stereo_set_separation(sep)
+    stereo_created = .true.
+
+    return
+  end subroutine stereo_create
 
 !
 ! Create the main window
@@ -222,7 +244,7 @@ contains
     ! Make a new window
     call gui_create_window(mainwin, dimensions=(/600,550/), &
          title="Gadgetviewer", statusbar=.true., menubar=.true.)
-    
+
     ! Set widgets to fill all available space
     call gui_packing_mode(expand=.true., fill=.true., spacing=3, &
          position=gui_start)
@@ -574,16 +596,6 @@ contains
     end do
 #endif
 
-    ! Add slider for stereo separation
-    call gui_create_box(stereo_box, toolbar, gui_horizontal)
-    call gui_create_label(label, stereo_box, "Stereo sep. ")
-    call gui_create_slider(stereo_slider, stereo_box, &
-         range=(/ stereo_min_sep, stereo_max_sep/), &
-         step=stereo_sep_step, orientation=gui_horizontal, min_size=100)
-    call gui_slider_set_value(stereo_slider,stereo_default_sep)
-    call gui_slider_get_value(stereo_slider,sep)
-    call stereo_set_separation(sep)
-
     ! Update state of controls at the bottom of the window
     call main_window_update_controls()
 
@@ -596,7 +608,6 @@ contains
     allocate(image(0:3*width*height-1), stat=istat)
     if(istat.ne.0)image = char(0)
     call main_window_redraw()
-    call gui_set_visible(stereo_box,.false.)
 
     is_fullscreen = .false.
 
@@ -1140,9 +1151,10 @@ contains
     endif
 
     ! Menu items for mono/stereo modes
-    if(  gui_menu_item_changed(view_mono).or.&
+    if(gui_menu_item_changed(view_mono).or.&
          gui_menu_item_changed(view_anaglyph).or.&
          gui_menu_item_changed(view_stereo))then
+       call stereo_create()
        call gui_menu_item_get_state(view_stereo, stereo_enabled)
        call gui_menu_item_get_state(view_anaglyph, anaglyph_enabled)
        call gui_set_visible(stereo_box, stereo_enabled.or.anaglyph_enabled)
@@ -1253,10 +1265,12 @@ contains
     endif
 
     ! Stereo separation slider
-    if(gui_slider_changed(stereo_slider))then
-       call gui_slider_get_value(stereo_slider, sep)
-       call stereo_set_separation(sep)
-       call main_window_redraw()
+    if(stereo_created)then
+       if(gui_slider_changed(stereo_slider))then
+          call gui_slider_get_value(stereo_slider, sep)
+          call stereo_set_separation(sep)
+          call main_window_redraw()
+       endif
     endif
 
     ! Movie option
@@ -1420,13 +1434,13 @@ contains
 
     ! Set menu checkboxes
     if(stereo_enabled)then
-       call gui_set_visible(stereo_box,.true.)
+       if(stereo_created)call gui_set_visible(stereo_box,.true.)
        call gui_menu_item_set_state(view_stereo, .true.)
     else if(anaglyph_enabled)then
-       call gui_set_visible(stereo_box,.true.)
+       if(stereo_created)call gui_set_visible(stereo_box,.true.)
        call gui_menu_item_set_state(view_anaglyph, .true.)
     else
-       call gui_set_visible(stereo_box,.false.)
+       if(stereo_created)call gui_set_visible(stereo_box,.false.)
        call gui_menu_item_set_state(view_mono, .true.)
     endif
     call gui_menu_item_set_state(view_auto_resample,  auto_resample)
